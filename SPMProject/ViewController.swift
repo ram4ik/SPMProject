09e7a6470c285bd589db98784f7e45f2bc571ef8
Ramil <ram4ik@hotmail.com>
2022-05-28 10:41:30 +0300

    onboarding ViewController

--- SPMProject/ViewController.swift
+++ SPMProject/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import OnboardingKit
 
 class ViewController: UIViewController {
 
+    private var onboardingKit: OnboardingKit?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        onboardingKit = OnboardingKit()
+        onboardingKit?.launchOnboarding(rootVC: self)
+    }
 }
-
