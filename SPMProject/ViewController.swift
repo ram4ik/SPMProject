2f53aa477014be20f275c76251cecaef3f87b15c
Ramil <ram4ik@hotmail.com>
2022-05-28 11:06:47 +0300

    create the skeleton: add framework dependency

--- SPMProject/ViewController.swift
+++ SPMProject/ViewController.swift
@@ -20,7 +20,16 @@ class ViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        onboardingKit = OnboardingKit()
+        onboardingKit = OnboardingKit(
+            slide: [
+                .init(image: UIImage(named: "slide1")!, title: "Start"),
+                .init(image: UIImage(named: "slide2")!, title: "Run"),
+                .init(image: UIImage(named: "slide3")!, title: "Inspire"),
+                .init(image: UIImage(named: "slide4")!, title: "More"),
+                .init(image: UIImage(named: "slide5")!, title: "Relax")
+            ],
+            tintColor: UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1.0)
+        )
         onboardingKit?.launchOnboarding(rootVC: self)
     }
 }
