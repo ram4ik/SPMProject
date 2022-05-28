029244ebecca3d63e7ffe07968225431c29aacca
Ramil <ram4ik@hotmail.com>
2022-05-28 20:38:48 +0300

    update title

--- SPMProject/ViewController.swift
+++ SPMProject/ViewController.swift
@@ -22,10 +22,10 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
         onboardingKit = OnboardingKit(
             slide: [
-                .init(image: UIImage(named: "slide1")!, title: "Start"),
+                .init(image: UIImage(named: "slide1")!, title: "New start"),
                 .init(image: UIImage(named: "slide2")!, title: "Run"),
                 .init(image: UIImage(named: "slide3")!, title: "Inspire"),
-                .init(image: UIImage(named: "slide4")!, title: "More"),
+                .init(image: UIImage(named: "slide4")!, title: "Be active"),
                 .init(image: UIImage(named: "slide5")!, title: "Relax")
             ],
             tintColor: UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1.0)
